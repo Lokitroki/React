64a13503950e634d64052a0e51f78806fee40d5b
Lokitroki <159892331+Lokitroki@users.noreply.github.com>
2024-05-22 10:13:39 +0200

    React icons
    Instalación React Icons e implementación en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import redes from './Footer.module.css'
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTwitch } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
 
 const Footer = () => {
   return (
     <footer>
         <div className={redes.socialMedia}>
-            <a href="https://www.facebook.com/UNIVERSAE/" target="_blank"><i class="fa fa-facebook-square"></i></a>
-            <a href="https://www.instagram.com/_universae/?hl=es" target="_blank"><i class="fa fa-instagram"></i></a>
-            <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><i class="fa fa-youtube-play"></i></a>
-            <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><i class="fa fa-twitch"></i></a>
-            <a href="https://www.linkedin.com/school/universae" target="_blank"><i class="fa fa-linkedin-square"></i></a>
-            <a href="https://www.tiktok.com/@_universae" target="_blank"><i class="fa fa-telegram"></i></a>
-            <a href="https://twitter.com/_Universae" target="_blank"><i class="fa fa-twitter"></i></a>
-            <a href="https://universae.com" target="_blank"><i class="fa fa-google"></i></a>
+          <ul>
+              <a href="https://www.facebook.com/UNIVERSAE/" target="_blank"><li><FaSquareFacebook/></li></a>
+              <a href="https://www.instagram.com/_universae/?hl=es" target="_blank"><li><FaInstagram/></li></a>
+              <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><li><FaYoutube/></li></a>
+              <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><li><FaTwitch/></li></a>
+              <a href="https://www.linkedin.com/school/universae" target="_blank"><li><FaLinkedin/></li></a>
+              <a href="https://www.tiktok.com/@_universae" target="_blank"><li><FaTiktok/></li></a>
+              <a href="https://twitter.com/_Universae" target="_blank"><li><FaTwitter/></li></a>
+              <a href="https://universae.com" target="_blank"><li><FaGoogle/></li></a>
+            </ul>
         </div>
     </footer>
   )
