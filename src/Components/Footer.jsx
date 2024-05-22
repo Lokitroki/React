42e65a4926923a43be0821f45cd15b55afc3f6a0
Lokitroki <159892331+Lokitroki@users.noreply.github.com>
2024-05-22 11:27:30 +0200

    New Icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,30 +1,22 @@
 import React from 'react'
 import redes from './Footer.module.css'
-import { FaSquareFacebook } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaTwitch } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaTiktok } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaGoogle } from "react-icons/fa";
+import { SocialIcon } from 'react-social-icons';
+import { Container } from 'react-bootstrap';
 
 
 const Footer = () => {
   return (
     <footer>
-        <div className={redes.socialMedia}>
-          <ul>
-              <a href="https://www.facebook.com/UNIVERSAE/" target="_blank"><li><FaSquareFacebook/></li></a>
-              <a href="https://www.instagram.com/_universae/?hl=es" target="_blank"><li><FaInstagram/></li></a>
-              <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><li><FaYoutube/></li></a>
-              <a href="https://www.youtube.com/c/UNIVERSAE_FP" target="_blank"><li><FaTwitch/></li></a>
-              <a href="https://www.linkedin.com/school/universae" target="_blank"><li><FaLinkedin/></li></a>
-              <a href="https://www.tiktok.com/@_universae" target="_blank"><li><FaTiktok/></li></a>
-              <a href="https://twitter.com/_Universae" target="_blank"><li><FaTwitter/></li></a>
-              <a href="https://universae.com" target="_blank"><li><FaGoogle/></li></a>
-            </ul>
-        </div>
+      <Container className={redes.redes}>
+        <SocialIcon className={redes.icons} url='https://www.facebook.com/UNIVERSAE/'/>
+        <SocialIcon className={redes.icons} url='https://www.instagram.com/_universae/?hl=es'/>
+        <SocialIcon className={redes.icons} url='https://www.youtube.com/c/UNIVERSAE_FP'/>
+        <SocialIcon className={redes.icons} url='https://www.youtube.com/c/UNIVERSAE_FP'/>
+        <SocialIcon className={redes.icons} url='https://www.linkedin.com/school/universae'/>
+        <SocialIcon className={redes.icons} url='https://www.tiktok.com/@_universae'/>
+        <SocialIcon className={redes.icons} url='https://twitter.com/_Universae'/>
+        <SocialIcon className={redes.icons} url='https://universae.com'/>
+      </Container>
     </footer>
   )
 }
